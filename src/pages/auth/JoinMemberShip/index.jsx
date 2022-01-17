5a5d22b684fdd4a85d168c2dcc236de36748e5c1
jae <goawmfhfl1@naver.com>
2022-01-17 17:08:43 +0900

    Add: Title & LayOut Component

--- src/pages/auth/JoinMemberShip/index.jsx
+++ src/pages/auth/JoinMemberShip/index.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { MainLayOut, MainTitle, Button } from "./index.style";
 
 const JoinMemberShip = () => {
   return (
     <>
-      <div>JoinMemberShip</div>
+      <MainLayOut>
+        <MainTitle>이메일로 회원가입</MainTitle>
+        {/* 지수님 Form Component */}
+        <Button>다음</Button>
+      </MainLayOut>
     </>
   );
 };
--- src/pages/auth/JoinMemberShip/index.style.js
+++ src/pages/auth/JoinMemberShip/index.style.js
@@ -6,7 +6,31 @@ export const MainLayOut = styled.div`
   align-items: center;
 `;
 
-export const FormContainer = styled.form``;
-export const InPutContainer = styled.div``;
-export const Button = styled.button``;
-export const JoinEmailBtn = styled.a``;
+export const MainTitle = styled.h2`
+  font-weight: 500;
+  font-size: 24px;
+  line-height: 30px;
+  margin: 30px 0 40px;
+  text-align: center;
+`;
+
+export const Button = styled.button`
+  /* 사이즈 */
+  width: 322px;
+  height: 44px;
+  border-radius: 44px;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: #ffc7a7;
+  color: #fff;
+  font-weight: 500;
+  font-size: 14px;
+  line-height: 18px;
+
+  /* 기본설정 */
+  border: none;
+  cursor: pointer;
+  padding: 0;
+`;
