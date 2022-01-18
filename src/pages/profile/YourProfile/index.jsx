d2c586315b8fd4dcb21a9f04ba30ac386d64f0e1
jae <goawmfhfl1@naver.com>
2022-01-18 17:54:00 +0900

    Add: YourProfile LayOut

--- src/pages/profile/YourProfile/index.jsx
+++ src/pages/profile/YourProfile/index.jsx
@@ -1,8 +1,31 @@
 import React from "react";
-import { MainLayOut } from "./index.style";
+import {
+  MainLayOut,
+  UserInfoContainer,
+  UserInfoWrapper,
+  ProductContainer,
+  ProductWrapper,
+  UserPostContainer,
+  UserPostWrapper,
+} from "./index.style";
+import DisplayHandler from "../../../components/DisplayHandler";
 
 const YourProfile = () => {
-  return <MainLayOut>YourProfile</MainLayOut>;
+  return (
+    <MainLayOut>
+      <UserInfoContainer>
+        <UserInfoWrapper></UserInfoWrapper>
+      </UserInfoContainer>
+      <ProductContainer>
+        <ProductWrapper></ProductWrapper>
+      </ProductContainer>
+      <UserPostContainer>
+        <UserPostWrapper>
+          <DisplayHandler />
+        </UserPostWrapper>
+      </UserPostContainer>
+    </MainLayOut>
+  );
 };
 
 export default YourProfile;
