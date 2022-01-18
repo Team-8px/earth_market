377ae455ad6f6964513211afb4b3f4a7aa4b74ec
jae <goawmfhfl1@naver.com>
2022-01-18 15:13:47 +0900

    Add: Post Component 구현

--- src/pages/home/HomeFeed/index.jsx
+++ src/pages/home/HomeFeed/index.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 //스타일 컴포넌트
 import { MainLayOut } from "./index.style";
+import Post from "../../../components/Post";
 
 const HomeFeed = () => {
-  return <MainLayOut>HomeMainLayOut</MainLayOut>;
+  return (
+    <MainLayOut>
+      <Post
+        authorId="테스트입니다"
+        authorName="this is test"
+        content="Content테스트 Content테스트 Content테스트 Content테스트 Content테스트 Content테스트 Content테스트 Content"
+        date="2022년 1월 18일"
+        likeCount="999"
+        commentCount="999"
+      />
+    </MainLayOut>
+  );
 };
 
 export default HomeFeed;
--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -13,6 +13,18 @@ const GlobalStyles = createGlobalStyle`
     body{
         font-size: 14px;
     }
+
+    img{
+        width: 100%;
+        height: auto;
+        vertical-align: top;
+    }
+    button {
+    border: none;
+    cursor: pointer;
+    padding: 0;
+    background-color: inherit;
+}
 `;
 
 export default GlobalStyles;
