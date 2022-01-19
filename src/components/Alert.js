9aa1c0064563d0c98c4dfa707a0efd96a32f2e0e
jae <goawmfhfl1@naver.com>
2022-01-20 08:21:04 +0900

    Add: AlertComponent
    컴포넌트 및 Alert기능 추가

---
+++ src/components/Alert.js
@@ -0,0 +1,111 @@
+import React, { useEffect, useState } from "react";
+import styled, { keyframes } from "styled-components";
+
+const slideUp = keyframes`
+from{
+  transform:translateY(100px)
+}
+to{
+  transform:translateY(0px)
+}
+`;
+
+const AlertModalContainer = styled.section`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 100;
+
+  animation-duration: 0.3s;
+  animation-timing-function: ease-out;
+  animation-name: ${slideUp};
+  animation-fill-mode: forwards;
+`;
+
+const AlertModalWrapper = styled.div`
+  width: 252px;
+  background: #fff;
+  border-radius: 10px;
+`;
+
+const AlertMessage = styled.strong`
+  display: block;
+  padding: 22px 0;
+  font-weight: 500;
+  font-size: 16px;
+  line-height: 20px;
+  text-align: center;
+`;
+
+const AlertMenuList = styled.ul`
+  display: flex;
+  width: 100%;
+  border-top: 0.5px solid #dbdbdb;
+
+  li {
+    width: 50%;
+  }
+`;
+
+const CancelBtn = styled.button`
+  width: 100%;
+  height: 46px;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 18px;
+`;
+
+const LogOutBtn = styled.button`
+  width: 100%;
+  height: 46px;
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 18px;
+  border-left: 0.5px solid #dbdbdb;
+  color: #f26e22;
+`;
+
+function Alert({ visible, cancelText, logoutText, offAlert }) {
+  const [animate, setAnimate] = useState(false);
+  const [localVisible, setLocalVisible] = useState();
+
+  useEffect(() => {
+    if (localVisible && !visible) {
+      setAnimate(true);
+      setTimeout(() => setAnimate(false), 100);
+    }
+    setLocalVisible(visible);
+  }, [localVisible, visible]);
+
+  if (!localVisible && !animate) return null;
+
+  return (
+    <AlertModalContainer>
+      <AlertModalWrapper>
+        <AlertMessage>로그아웃 하시겠어요?</AlertMessage>
+        <AlertMenuList>
+          <li>
+            <CancelBtn type="button" onClick={offAlert}>
+              {cancelText}
+            </CancelBtn>
+          </li>
+          <li>
+            <LogOutBtn type="button">{logoutText}</LogOutBtn>
+          </li>
+        </AlertMenuList>
+      </AlertModalWrapper>
+    </AlertModalContainer>
+  );
+}
+
+Alert.defaultProps = {
+  cancelText: "취소",
+  logoutText: "로그아웃",
+};
+
+export default Alert;
--- src/pages/profile/MyProfile/index.jsx
+++ src/pages/profile/MyProfile/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector, shallowEqual } from "react-redux";
 import { Link } from "react-router-dom";
 import { listProducts } from "../../../actions/productActions";
@@ -16,6 +16,7 @@ import {
   UserPostWrapper,
 } from "./index.style";
 import DisplayHandler from "../../../components/DisplayHandler";
+import Alert from "../../../components/Alert";
 
 const MyProfile = () => {
   const dispatch = useDispatch();
@@ -25,6 +26,27 @@ const MyProfile = () => {
   const { image, username, accountname, intro, followerCount, followingCount } =
     useSelector(state => state.userProfile, shallowEqual);
 
+  const [dialog, setDialog] = useState(false);
+  const [alert, setAlert] = useState(false);
+
+  const onAlert = () => {
+    console.log("확인");
+    setAlert(true);
+  };
+  const offAlert = () => {
+    console.log("취소");
+    setAlert(false);
+  };
+
+  const onDialog = () => {
+    console.log("확인");
+    setDialog(true);
+  };
+  const offDialog = () => {
+    console.log("취소");
+    setDialog(false);
+  };
+
   useEffect(() => {
     dispatch(listProducts());
     dispatch(listPosts());
@@ -32,83 +54,86 @@ const MyProfile = () => {
   }, [dispatch]);
 
   return (
-    <MainLayOut>
-      {/* 유저 프로필 */}
-      <UserInfoContainer>
-        <UserInfoWrapper>
-          <h1>유저 프로필</h1>
-          <img src={image} alt="프로필 사진" />
-          <ul>
-            <li>{username}</li>
-            <li>{accountname}</li>
-            <li>{intro}</li>
-            <li>{followerCount}</li>
-            <li>{followingCount}</li>
-          </ul>
-        </UserInfoWrapper>
-      </UserInfoContainer>
-      {/* 상품목록 */}
-      <ProductContainer>
-        <ProductWrapper>
-          <h1>상품 목록</h1>
-          {products &&
-            products.map((product, index) => {
-              return (
-                <div key={index} style={{ display: "flex" }}>
-                  <div>
-                    <img
-                      src={`${API_URL}/${product.itemImage}`}
-                      alt="상품사진"
-                    />
+    <>
+      <MainLayOut>
+        {/* 유저 프로필 */}
+        <UserInfoContainer>
+          <UserInfoWrapper>
+            <h1>유저 프로필</h1>
+            <img src={image} alt="프로필 사진" />
+            <ul>
+              <li>{username}</li>
+              <li>{accountname}</li>
+              <li>{intro}</li>
+              <li>{followerCount}</li>
+              <li>{followingCount}</li>
+            </ul>
+          </UserInfoWrapper>
+        </UserInfoContainer>
+        {/* 상품목록 */}
+        <ProductContainer>
+          <ProductWrapper>
+            <h1>상품 목록</h1>
+            {products &&
+              products.map((product, index) => {
+                return (
+                  <div key={index} style={{ display: "flex" }}>
+                    <div>
+                      <img
+                        src={`${API_URL}/${product.itemImage}`}
+                        alt="상품사진"
+                      />
+                    </div>
+                    <div>
+                      <ul>
+                        <li>상품명: {product.itemName}</li>
+                        <li>상품가격: {product.price}</li>
+                        <li>상품링크: {product.link}</li>
+                      </ul>
+                    </div>
                   </div>
-                  <div>
-                    <ul>
-                      <li>상품명: {product.itemName}</li>
-                      <li>상품가격: {product.price}</li>
-                      <li>상품링크: {product.link}</li>
-                    </ul>
-                  </div>
-                </div>
-              );
-            })}
-        </ProductWrapper>
-      </ProductContainer>
-      {/* 게시글 목록 */}
-      <UserPostContainer>
-        <UserPostWrapper>
-          <DisplayHandler></DisplayHandler>
-          <h1>게시글 목록</h1>
-          {posts &&
-            posts.map(post => {
-              /* 여러개의 게시글 이미지를 여러 개의 문자열로 배열에 담아 나눔 */
-              const postImages = post.image.split(",");
+                );
+              })}
+          </ProductWrapper>
+        </ProductContainer>
+        {/* 게시글 목록 */}
+        <UserPostContainer>
+          <UserPostWrapper>
+            <DisplayHandler></DisplayHandler>
+            <h1>게시글 목록</h1>
+            {posts &&
+              posts.map(post => {
+                /* 여러개의 게시글 이미지를 여러 개의 문자열로 배열에 담아 나눔 */
+                const postImages = post.image.split(",");
 
-              return (
-                <div key={post.id}>
-                  <div style={{ display: "flex" }}>
-                    {postImages.map((postImage, index) => {
-                      return (
-                        <img
-                          key={index}
-                          src={`${API_URL}/${postImage}`}
-                          alt="상품사진"
-                        />
-                      );
-                    })}
+                return (
+                  <div key={post.id}>
+                    <div style={{ display: "flex" }}>
+                      {postImages.map((postImage, index) => {
+                        return (
+                          <img
+                            key={index}
+                            src={`${API_URL}/${postImage}`}
+                            alt="상품사진"
+                          />
+                        );
+                      })}
+                    </div>
+                    <div>
+                      <ul>
+                        <li>
+                          <Link to={`/post${post.id}`}>게시글 링크</Link>
+                        </li>
+                      </ul>
+                    </div>
                   </div>
-                  <div>
-                    <ul>
-                      <li>
-                        <Link to={`/post${post.id}`}>게시글 링크</Link>
-                      </li>
-                    </ul>
-                  </div>
-                </div>
-              );
-            })}
-        </UserPostWrapper>
-      </UserPostContainer>
-    </MainLayOut>
+                );
+              })}
+          </UserPostWrapper>
+        </UserPostContainer>
+      </MainLayOut>
+      <Alert visible={alert} offAlert={offAlert} />
+    </>
   );
 };
 
