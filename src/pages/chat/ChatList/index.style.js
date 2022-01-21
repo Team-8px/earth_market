2b9b4770a8fa648d10bc046d79ecef6051c27f89
jisu2433 <93359953+jisu2433@users.noreply.github.com>
2022-01-21 16:50:45 +0900

    Add : ChatList 디자인 추가
    1. ChatList CSS 추가했습니다.
    2. ChatList User 부분 디자인을 위함입니다.

--- src/pages/chat/ChatList/index.style.js
+++ src/pages/chat/ChatList/index.style.js
@@ -2,3 +2,56 @@ import styled from "styled-components";
 import { ChatListMainLayOut } from "../../../components/MainLayOut";
 
 export const MainLayOut = styled(ChatListMainLayOut)``;
+
+export const ChatListContainer = styled.ul`
+    width: 100%;
+    max-width: 390px;
+`;
+
+export const ChatItemContainer = styled.li`
+    position: relative;
+    display: flex;
+    align-items: center;
+    margin-bottom: 20px;
+    cursor: pointer;
+`;
+
+export const UserImgWrapper = styled.div`
+    width: 42px;
+    height: 42px;
+    border: 0.5px solid #dbdbdb;
+    border-radius: 50%;
+    margin-right: 12px;
+
+    img{
+        height: 100%;
+        object-fit: cover;
+        border-radius: 50%;
+    }
+`;
+
+export const UserInfoWrapper = styled.strong`
+    display: block;
+    font-size: 14px;
+    font-weight: 400;
+    line-height: 18px;
+    margin: 2px 0 4px 0;
+    
+    p{
+        max-width: 238px;
+        font-weight: 400;
+        font-size: 12px;
+        line-height: 15px;
+        color: #767676;
+    }
+`;
+
+export const DateContainer = styled.strong`
+    position: absolute;
+    right: 0;
+    bottom: 3px;
+    font-weight: 400;
+    font-size: 10px;
+    line-height: 13px;
+    color: dbdbdb;
+`;
