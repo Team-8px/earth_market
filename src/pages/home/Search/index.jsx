d92e0e0a92ab5b93093961cd537ad77cb222876c
jisu2433 <93359953+jisu2433@users.noreply.github.com>
2022-01-21 16:47:01 +0900

    Add : UserList 디자인 추가
    1.  UserList CSS 추가했습니다.
    2. Search 페이지 디자인을 위함입니다.

--- src/pages/home/Search/index.jsx
+++ src/pages/home/Search/index.jsx
@@ -1,13 +1,20 @@
 import React from "react";
 import { HeaderSearch } from "../../../components/Header";
-import { MainLayOut } from "./index.style";
+import { MainLayOut, UserListContainer, UserListWrapper, UserInfoWrapper, UserName, UserId } from "./index.style";
 
 const Search = () => {
   return (
-    // <MainLayOut>
+    <MainLayOut>
       <HeaderSearch />
+      <UserListContainer>
+      <UserListWrapper></UserListWrapper>
+      <UserInfoWrapper>
+        <UserName></UserName>
+        <UserId></UserId>
+      </UserInfoWrapper>
+      </UserListContainer>
       // Search
-    // </MainLayOut>
+    </MainLayOut>
   );
 };
 
