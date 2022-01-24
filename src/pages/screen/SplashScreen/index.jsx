9beec6bd2466a5f8ec000aba78615822e47f2e5b
mijin <mmcho122@gmail.com>
2022-01-24 15:42:06 +0900

    Add: SplashScreen 디자인 추가

--- src/pages/screen/SplashScreen/index.jsx
+++ src/pages/screen/SplashScreen/index.jsx
@@ -1,8 +1,21 @@
 import React from "react";
-import { PageLayOut } from "./index.style";
+// import { PageLayOut } from "./index.style";
+import { SplashScreenLayOut } from "../../../components/MainLayOut";
+import SplashLogo from "../../../asset/icon/full-logo.svg";
+import styled from "styled-components";
 
 const SplashScreen = () => {
-  return <PageLayOut>SplashScreen</PageLayOut>;
+  return (
+    // <PageLayOut>
+        <SplashScreenLayOut>
+          <SplashImg src={SplashLogo} />
+        </SplashScreenLayOut>
+    // </PageLayOut>
+  );
 };
 
 export default SplashScreen;
+
+const SplashImg = styled.img`
+  width: 200px;
+`;
