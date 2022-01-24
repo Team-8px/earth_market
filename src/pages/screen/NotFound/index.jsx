327149102fcb5b2043679aae21626f8e67ccd365
mijin <mmcho122@gmail.com>
2022-01-24 15:41:54 +0900

    Add: NotFound 디자인 추가

--- src/pages/screen/NotFound/index.jsx
+++ src/pages/screen/NotFound/index.jsx
@@ -1,8 +1,33 @@
 import React from "react";
-import { PageLayOut } from "./index.style";
+import styled from "styled-components";
+// import { PageLayOut } from "./index.style";
+import { NotFoundLayOut } from "../../../components/MainLayOut";
+import NotFoundIcon from "../../../asset/icon/icon-404.svg"
+// import { Button } from "../../../components/Button";
 
 const NotFound = () => {
-  return <PageLayOut>NotFound</PageLayOut>;
+  return (
+    // <PageLayOut>
+      <NotFoundLayOut>
+        <NotFoundImg src={NotFoundIcon} />
+        <LoginEmailTxt>페이지를 찾을 수 없습니다. :(</LoginEmailTxt>
+        {/* <Button /> */}
+      </NotFoundLayOut>
+    // </PageLayOut>
+  );
 };
 
 export default NotFound;
+
+const LoginEmailTxt = styled.span`
+  font-weight: 400;
+  font-size: 14px;
+  line-height: 15px;
+  color: #767676;
+  text-align: center;
+`;
+
+const NotFoundImg = styled.img`
+  width: 158px;
+  margin-bottom: 32px;
+`;
