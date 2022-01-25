339ff1de53b7dce788fdb8da32365272e1d9afe6
jisu2433 <93359953+jisu2433@users.noreply.github.com>
2022-01-25 16:54:04 +0900

    Add : ChatRoom 스타일링
    1. ChatRoom 스타일 추가했습니다.
    2. 채팅 부분 스타일링을 위함입니다.

--- src/pages/chat/ChatRoom/index.jsx
+++ src/pages/chat/ChatRoom/index.jsx
@@ -1,12 +1,22 @@
 import React from "react";
 import { HeaderChat } from "../../../components/Header";
-import { MainLayOut } from "./index.style";
-
+import { MainLayOut, ChatRoomLayOut,ChatRoomContainer, UserImgWrapper, UserMsgWrapper} from "./index.style";
+import EllipseImg from "../../../asset/Ellipse-1.png";
 const ChatRoom = () => {
   return (
     <MainLayOut>
       <HeaderChat />
-      ChatRoom
+      <ChatRoomLayOut>
+      <ChatRoomContainer>
+        <UserImgWrapper>
+          <img src={EllipseImg} alt="프로필 사진" />
+        </UserImgWrapper>
+        <UserMsgWrapper>
+          <p>옷을 인생을 그러므로 없으면 것은 이상은 것은 우리의 위하여, 뿐이다. 이상의 청춘의 뼈 따뜻한 그들의 그와 약동하다. 대고, 못할 넣는 풍부하게 뛰노는 인생의 힘있다.</p>
+          <strong>9:22</strong>
+        </UserMsgWrapper>
+      </ChatRoomContainer>
+      </ChatRoomLayOut>
     </MainLayOut>
   );
 };
