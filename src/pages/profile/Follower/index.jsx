170c8e3d305043404538f05ca19c0a2789e91918
jisu2433 <93359953+jisu2433@users.noreply.github.com>
2022-01-25 16:41:09 +0900

    Add : Follower 컴포넌트 추가
    1. Follower 컴포넌트 추가하였습니다.
    2. Follower 스타일링을 위함입니다.

--- src/pages/profile/Follower/index.jsx
+++ src/pages/profile/Follower/index.jsx
@@ -1,16 +1,13 @@
 import React from "react";
 import { FolloMainLayOut } from "../../../components/MainLayOut";
 import { HeaderFollowList } from "../../../components/Header";
-import { UserListContainer, UserListWrapper, UserInfoWrapper } from "./index.style";
+import FollowerList from "../../../components/FollowerList";
 
 const Follower = () => {
   return (
     <FolloMainLayOut>
       <HeaderFollowList />
-      <UserListContainer>
-        <UserListWrapper></UserListWrapper>
-        <UserInfoWrapper></UserInfoWrapper>
-      </UserListContainer>
+        <FollowerList />
     </FolloMainLayOut>
   );
 };
