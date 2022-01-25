16759e024ac931399683a1f06676ff727c4bcd39
jae <goawmfhfl1@naver.com>
2022-01-25 13:21:26 +0900

    Add: PostView 컴포넌트 모달 및 Alert 추가
    - HiddenMenu
    - Post
    - Chat

--- src/pages/post/PostView/index.jsx
+++ src/pages/post/PostView/index.jsx
@@ -7,6 +7,8 @@ import { getCommentList } from "../../../actions/commentAction";
 import { API_URL } from "../../../constants/defaultUrl";
 import { MainLayOut } from "../PostUpload/index.style";
 import { CommentWrapper } from "./index.style";
+import { Alert, AlertButton } from "../../../components/Alert";
+import { HiddenMenu, ListBtn, AlertBtn } from "../../../components/HiddenMenu";
 import Post from "../../../components/Post";
 import { HeaderBasic } from "../../../components/Header";
 // 댓글 생성 부분 컴포넌트만 다시 불러와서 적용할 예정입니다.
@@ -30,6 +32,26 @@ const PostView = () => {
     shallowEqual,
   );
 
+  // 스타일 컴포넌트 관련 상태관리
+
+  // 게시글 메뉴
+  const [postDialog, setPostDialog] = useState(false);
+  const [postAlert, setPostAlert] = useState(false);
+  const isPostDialog = () => setPostDialog(!postDialog);
+  const isPostAlert = () => setPostAlert(!postAlert);
+
+  // 네비게이션 메뉴
+  const [navDialog, setNavDialog] = useState(false);
+  const [navAlert, setNavAlert] = useState(false);
+  const isNavDialog = () => setNavDialog(!navDialog);
+  const isNavAlert = () => setNavAlert(!navAlert);
+
+  // 댓글 메뉴
+  const [chatDialog, setChatDialog] = useState(false);
+  const [chatAlert, setChatAlert] = useState(false);
+  const isChatDialog = () => setChatDialog(!chatDialog);
+  const isChatAlert = () => setChatAlert(!chatAlert);
+
   useEffect(() => {
     dispatch(getUserMyProfile());
   }, [dispatch]);
@@ -47,19 +69,100 @@ const PostView = () => {
   };
 
   return (
-    <MainLayOut>
-      <HeaderBasic />
-      <CommentWrapper>
-        <Post
-          authorId="테스트입니다"
-          authorName="this is test"
-          content="Content테스트 Content테스트 Content테스트 Content테스트 Content테스트 Content테스트 Content테스트 Content"
-          date="2022년 1월 18일"
-          likeCount="999"
-          commentCount="999"
-        />
-      </CommentWrapper>
-    </MainLayOut>
+    <>
+      <MainLayOut>
+        <HeaderBasic isDialog={isNavDialog} />
+        <CommentWrapper>
+          <Post
+            authorId="테스트입니다"
+            authorName="this is test"
+            content="Content테스트 Content테스트 Content테스트 Content테스트 Content테스트 Content테스트 Content테스트 Content"
+            date="2022년 1월 18일"
+            likeCount="999"
+            commentCount="999"
+            isDialog={isPostDialog}
+          />
+        </CommentWrapper>
+        <button onClick={isChatDialog}>테스트</button>
+      </MainLayOut>
+      {/* 🏞 게시글 Modal */}
+      <HiddenMenu visible={postDialog}>
+        <li>
+          <AlertBtn type="button" isAlert={isPostAlert}>
+            신고하기
+          </AlertBtn>
+        </li>
+        <li>
+          <ListBtn type="button" isDialog={isPostDialog}>
+            모달창 닫기
+          </ListBtn>
+        </li>
+      </HiddenMenu>
+      {/* 🏞 게시글 Alert */}
+      <Alert visible={postAlert} messageText="게시글을 신고하시겠어요?">
+        <li>
+          <AlertButton type="button" isAlert={isPostAlert}>
+            취소
+          </AlertButton>
+        </li>
+        <li>
+          <AlertButton type="button">신고</AlertButton>
+        </li>
+      </Alert>
+      {/* 🕹 Nav Modal */}
+      <HiddenMenu visible={navDialog}>
+        <li>
+          <ListBtn type="button" visible={navDialog}>
+            설정 및 개인정보
+          </ListBtn>
+        </li>
+        <li>
+          <AlertBtn type="button" isAlert={isNavAlert}>
+            로그아웃
+          </AlertBtn>
+        </li>
+        <li>
+          <ListBtn type="button" isDialog={isNavDialog}>
+            임시 모달 창 닫기 버튼
+          </ListBtn>
+        </li>
+      </HiddenMenu>
+      {/* 🕹 Nav Alert */}
+      <Alert visible={navAlert} messageText="로그아웃 하시겠어요?">
+        <li>
+          <AlertButton type="button" isAlert={isNavAlert}>
+            취소
+          </AlertButton>
+        </li>
+        <li>
+          <AlertButton type="button">로그아웃</AlertButton>
+        </li>
+      </Alert>
+      {/* 💬 Comment Modal */}
+      <HiddenMenu visible={chatDialog}>
+        <li>
+          <AlertBtn type="button" isAlert={isChatAlert}>
+            신고하기
+          </AlertBtn>
+        </li>
+        <li>
+          <ListBtn type="button" isDialog={isChatDialog}>
+            모달창 닫기
+          </ListBtn>
+        </li>
+      </HiddenMenu>
+      {/* 💬 Comment Alert */}
+      <Alert visible={chatAlert} messageText="이 사용자를 신고하시겠어요?">
+        <li>
+          <AlertButton type="button" isAlert={isChatAlert}>
+            취소
+          </AlertButton>
+        </li>
+        <li>
+          <AlertButton type="button">신고</AlertButton>
+        </li>
+      </Alert>
+    </>
   );
 };
 
