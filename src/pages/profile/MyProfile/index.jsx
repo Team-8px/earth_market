dbdcad1552e635cb14824aa70e354ba0ee08f05d
jae <goawmfhfl1@naver.com>
2022-01-25 12:57:12 +0900

    Edit: 현길님 임시 작업 제거

--- src/pages/profile/MyProfile/index.jsx
+++ src/pages/profile/MyProfile/index.jsx
@@ -54,29 +54,7 @@ const MyProfile = () => {
         <UserInfo></UserInfo>
         {/* 상품목록 */}
         <ProductContainer>
-          <ProductWrapper>
-            <h1>상품 목록</h1>
-            {products &&
-              products.map((product, index) => {
-                return (
-                  <div key={index} style={{ display: "flex" }}>
-                    <div>
-                      <img
-                        src={`${API_URL}/${product.itemImage}`}
-                        alt="상품사진"
-                      />
-                    </div>
-                    <div>
-                      <ul>
-                        <li>상품명: {product.itemName}</li>
-                        <li>상품가격: {product.price}</li>
-                        <li>상품링크: {product.link}</li>
-                      </ul>
-                    </div>
-                  </div>
-                );
-              })}
-          </ProductWrapper>
+          <ProductWrapper></ProductWrapper>
         </ProductContainer>
         {/* 게시글 목록 */}
         <UserPostContainer>
@@ -166,40 +144,5 @@ const MyProfile = () => {
     </>
   );
 };
-
-/**
-  * @products : 상품들 array
-  [
-    {
-      id: "61db8a3c209fe0d5be04ad5b"
-      itemImage: "1641777724819.jpg"
-      itemName: "당근"
-      link: "www.naver.com"
-      price: 10,000,
-      author : {}
-    }, ....
-  ]
-  * @posts : 게시글들 array
-  [
-    {
-      commentCount: 0
-      comments: []
-      content: "안녕하세요"
-      createdAt: "2022-01-11T10:37:25.497Z"
-      heartCount: 0
-      hearted: false
-      id: "61dd5de53fe886cd13383917"
-      image: "1641897445443.jpg,1641897445466.jpg,1641897445471.jpg",
-      author : {}
-    }, ...
-  ]
-  
-  * @postImages : 게시글 이미지들 array
-  [
-    '1641802294157.jpg', 
-    '1641802294183.jpg', 
-    '1641802294203.jpg'
-  ]
-*/
 
 export default MyProfile;
