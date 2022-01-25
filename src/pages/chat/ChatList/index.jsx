71680c74b0d08f51d4d3d954d06aa9faaa3dacc2
jisu2433 <93359953+jisu2433@users.noreply.github.com>
2022-01-25 16:44:01 +0900

    Add : ChatInfoList 컴포넌트 추가
    1. ChatInfoList 컴포넌트를 추가했습니다.
    2. ChatList 스타일링을 위함입니다.

--- src/pages/chat/ChatList/index.jsx
+++ src/pages/chat/ChatList/index.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { HeaderBasic } from "../../../components/Header";
 import { MainLayOut } from "./index.style";
+import ChatInfo from "../../../components/ChatInfoList";
 
 const ChatList = () => {
   return (
     <MainLayOut>
       <HeaderBasic />
-      ChatList
+      <ChatInfo/>
     </MainLayOut>
   )
 };
