e3da09a9c10b3b74a9cdaca127b568e9769314f4
jae <goawmfhfl1@naver.com>
2022-01-26 12:02:45 +0900

    Fix: Nav와 MainLayout 분리

--- src/pages/home/Search/index.jsx
+++ src/pages/home/Search/index.jsx
@@ -1,20 +1,28 @@
 import React from "react";
 import { HeaderSearch } from "../../../components/Header";
-import { MainLayOut, UserListContainer, UserListWrapper, UserInfoWrapper, UserName, UserId } from "./index.style";
+import {
+  MainLayOut,
+  UserListContainer,
+  UserListWrapper,
+  UserInfoWrapper,
+  UserName,
+  UserId,
+} from "./index.style";
 
 const Search = () => {
   return (
-    <MainLayOut>
+    <>
       <HeaderSearch />
-      <UserListContainer>
-      <UserListWrapper></UserListWrapper>
-      <UserInfoWrapper>
-        <UserName></UserName>
-        <UserId></UserId>
-      </UserInfoWrapper>
-      </UserListContainer>
-      // Search
-    </MainLayOut>
+      <MainLayOut>
+        <UserListContainer>
+          <UserListWrapper></UserListWrapper>
+          <UserInfoWrapper>
+            <UserName></UserName>
+            <UserId></UserId>
+          </UserInfoWrapper>
+        </UserListContainer>
+      </MainLayOut>
+    </>
   );
 };
 
