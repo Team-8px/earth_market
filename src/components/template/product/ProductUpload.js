75d11882c8ad513fcdbe630405ec89ab9eec24e3
jisu2433 <93359953+jisu2433@users.noreply.github.com>
2022-02-06 15:30:43 +0900

    Add : ProductForm 추가

--- src/components/template/product/ProductUpload.js
+++ src/components/template/product/ProductUpload.js
@@ -1,11 +1,13 @@
 import React from "react";
 import styled from "styled-components";
+import ProductForm from "../../module/form/ProductForm";
 
 export default function ProductUpload() {
-  return <div>ProductUpload</div>;
+  return (
   // <Header></Header>
   // <LayOut>
   //   <ProductUpload></ProductUpload>
-  //   <ProductForm></ProductForm>
+    <ProductForm></ProductForm>
   // </LayOut>
+  )
 }
