f734f64c357a9007d5e2bb8abf952d787b853ab2
jisu2433 <93359953+jisu2433@users.noreply.github.com>
2022-02-06 20:06:36 +0900

    Add : ProfileUpload 추가

--- src/components/template/profile/ProfileEdit.js
+++ src/components/template/profile/ProfileEdit.js
@@ -1,13 +1,14 @@
 import React from "react";
 import styled from "styled-components";
+import ProfileUpload from "../../module/profile/ProfileIUpload";
 import ProfileForm from "../../module/form/ProfileForm";
 
 export default function ProfileEdit() {
   return (
-  // <Header></Header>
-  // <LayOut>
-  //   <ProfileUpload></ProfileUpload>
-    <ProfileForm></ProfileForm>
-  // </LayOut>
+    // <Header></Header>
+    // <LayOut>
+    <ProfileUpload></ProfileUpload>
+    // <ProfileForm></ProfileForm>
+    // </LayOut>
   );
 }
