9ae2e593a921a7eb08a1a7ae47ce339e82253c6a
jisu2433 <93359953+jisu2433@users.noreply.github.com>
2022-02-07 13:09:53 +0900

    Add : FollowersUser 추가

--- src/components/template/profile/Follower.js
+++ src/components/template/profile/Follower.js
@@ -1,16 +1,18 @@
 import React from "react";
 import styled from "styled-components";
+import FollowersUser from "../../module/user/FollowersUser";
 
 const LayOut = styled.div``;
 const List = styled.div``;
 
 export default function Follower() {
-  return <div>Follower</div>;
+  return (
   // <Header></Header>
   // <LayOut>
   //   <List>
-  //     <FollowersUser></FollowersUser>
+      <FollowersUser></FollowersUser>
   //   </List>
   // </LayOut>
   // <Navigation></Navigation>
+  );
 }
