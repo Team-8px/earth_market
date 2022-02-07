da91f5054ffe8f2c16d294541b8b5d1bc4e0e4a1
jisu2433 <93359953+jisu2433@users.noreply.github.com>
2022-02-07 23:16:56 +0900

    Add : Chat 추가

--- src/components/template/chat/ChatRoom.js
+++ src/components/template/chat/ChatRoom.js
@@ -1,11 +1,13 @@
 import React from "react";
 import styled from "styled-components";
+import Chat from "../../module/chat/Chat";
 
 export default function ChatRoom() {
-  return <div>ChatRoom</div>;
-  /* <Header></Header>
-      <LayOut>
-        <Chat></Chat>
-      </LayOut>
-      <Comment></Comment> */
+  return (
+    // <Header></Header>
+    // <LayOut>
+    <Chat></Chat>
+    // </LayOut>
+    // <Comment></Comment> */
+  );
 }
