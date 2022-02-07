e5453c51357f13eb1ee7b6cf4d8c554b39dc9ad9
jisu2433 <93359953+jisu2433@users.noreply.github.com>
2022-02-07 22:51:09 +0900

    Add : ReplyBox 추가

--- src/components/template/post/PostView.js
+++ src/components/template/post/PostView.js
@@ -1,23 +1,24 @@
 import React from "react";
 import styled from "styled-components";
-
+import ReplyBox from "../../module/post/ReplyBox";
 const PostLayOut = styled.div``;
 const List = styled.div``;
 
 export default function PostView() {
-  return <div>PostView</div>;
-  /* <Header></Header>
-      게시글
-      <PostLayOut>
-        <UserInfoBox></UserInfoBox>
-        <ContentBox></ContentBox>
-        <IconBox></IconBox>
-        <Date></Date>
-      </PostLayOut>
-      댓글 창
-      <List>
+  return (
+      // <Header></Header>
+      // 게시글
+      // <PostLayOut>
+      //   <UserInfoBox></UserInfoBox>
+      //   <ContentBox></ContentBox>
+      //   <IconBox></IconBox>
+      //   <Date></Date>
+      // </PostLayOut>
+      // 댓글 창
+      // <List>
         <ReplyBox></ReplyBox>
-      </List>
-      댓글 입력창
-      <ConmentBox></ConmentBox> */
+      // </List>
+      // 댓글 입력창
+      // <ConmentBox></ConmentBox>
+  );
 }
