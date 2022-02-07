2459f5c6c77baf4d23c76dfa1021a626e5d39729
jisu2433 <93359953+jisu2433@users.noreply.github.com>
2022-02-07 14:04:40 +0900

    Add : ChatListUser 추가

--- src/components/template/chat/ChatList.js
+++ src/components/template/chat/ChatList.js
@@ -1,15 +1,16 @@
 import React from "react";
 import styled from "styled-components";
-
+import ChatListUser from "../../module/user/ChatListUser";
 const LayOut = styled.div``;
 const List = styled.div``;
 
 export default function ChatList() {
-  return <div>ChatList</div>;
-  /* <Header></Header>
-      <LayOut>
-        <List>
-          <ChatListUser></ChatListUser>
-        </List>
-      </LayOut> */
+  return(
+    // <Header></Header>
+    //   <LayOut>
+    //   <List>
+            <ChatListUser></ChatListUser>
+    //   </List>
+    //   </LayOut>
+  );
 }
