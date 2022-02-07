9ad466a92260b4462d1f0dceb087e147912045dc
jisu2433 <93359953+jisu2433@users.noreply.github.com>
2022-02-07 22:57:36 +0900

    Add : UserInfo 컴포넌트 적용

--- src/components/template/profile/Profile.js
+++ src/components/template/profile/Profile.js
@@ -1,14 +1,14 @@
 import React from "react";
 import styled from "styled-components";
-
+import UserInfo from "../../UserInfo";
 // myProfile 부분입니다.
 // yourProfile 부분입니다.
 const LayOut = styled.div``;
 
 export default function Profile() {
-  return <div>Profile</div>;
+  return (
   // <Header></Header>
-  // 프로필
+    <UserInfo></UserInfo>
   // <Profile></Profile>
   // 상품
   // <Product></Product>
@@ -19,4 +19,5 @@ export default function Profile() {
   //   <Post></Post>
   // </LayOut>
   // <Navigation></Navigation>
+  );
 }
