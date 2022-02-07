0599da81243c4558c6efc593f1b6effb652de8aa
jae <goawmfhfl1@naver.com>
2022-02-07 14:32:52 +0900

    Add: ChatList 작업

--- src/components/template/chat/ChatList.js
+++ src/components/template/chat/ChatList.js
@@ -1,15 +1,27 @@
 import React from "react";
 import styled from "styled-components";
 
-const LayOut = styled.div``;
-const List = styled.div``;
-
 export default function ChatList() {
-  return <div>ChatList</div>;
-  /* <Header></Header>
+  return (
+    <>
+      {/* <Header></Header> */}
       <LayOut>
-        <List>
-          <ChatListUser></ChatListUser>
-        </List>
-      </LayOut> */
+        {/* <List> */}
+        {/* <ChatListUser></ChatListUser> */}
+        {/* </List> */}
+      </LayOut>
+    </>
+  );
 }
+const LayOut = styled.main`
+  min-width: 390px;
+  width: 100%;
+  height: 100%;
+  margin-top: 48px;
+  display: flex;
+  justify-content: center;
+  position: fixed;
+  padding: 24px 16px;
+  overflow-y: scroll;
+`;
+const List = styled.div``;
