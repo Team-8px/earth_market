23f6988749077bf5dc29fef89c8c9e86ef2fb63e
mijin <mmcho122@gmail.com>
2022-02-09 04:23:24 +0900

    Add: LoginTilte 컴포넌트 추가

--- src/components/template/join/JoinProfile.js
+++ src/components/template/join/JoinProfile.js
@@ -3,13 +3,16 @@ import styled from "styled-components";
 import { Button } from "../../module/button/Button";
 import ProfileForm from "../../module/form/ProfileForm";
 import ProfileUpload from "../../module/profile/ProfileIUpload";
+import LoginTitle, { SubTitleTitle } from "../../module/title/LoginTitle";
 
 const JoinProfile = () => {
   return (
     <>
       <LayOut>
-        {/* <ProfileTitle></ProfileTitle> */}
-        {/* <SubTitle></SubTitle> */}
+        <LoginTitle>
+          프로필 설정
+          <SubTitleTitle />
+        </LoginTitle>
         <ProfileUpload></ProfileUpload>
         <ProfileForm></ProfileForm>
         <Button width="322px" size="lg" off>
