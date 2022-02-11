b68517ec575583bc033439da1aceb0a9e7b95b39
jae <goawmfhfl1@naver.com>
2022-02-11 18:11:20 +0900

    Add: PostView

--- src/PAGE/PostView.jsx
+++ src/PAGE/PostView.jsx
@@ -1,13 +1,38 @@
-import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { useForm } from "react-hook-form";
+import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { getUserMyProfile } from "../actions/userActions";
 import { getPost } from "../actions/postActions";
-import { getCommentList, deleteComment } from "../actions/commentAction";
+import {
+  getCommentList,
+  deleteComment,
+  commentCreateAction,
+} from "../actions/commentAction";
 //import Comment from "../../../components/Comment-gh";
 
+// 스타일로직
+import UserInfoBox from "../components/module/post/UserInfoBox";
+import {
+  ContentBox,
+  ImgContainer,
+  ButtonList,
+  ImgList,
+} from "../components/module/post/ContentBox";
+import { Button } from "../components/module/button/button";
+import { Modal, AlertBtn, ListBtn } from "../components/module/modal/Modal";
+import { Alert, AlertBox } from "../components/module/alert/Alert";
+import { HeaderHome } from "../components/template/common/Header";
+import IconBox from "../components/module/post/IconBox";
+import Date from "../components/module/post/Date";
+import more from "../asset/icon-more-vertical.svg";
+import styled, { css } from "styled-components";
+import dayjs from "dayjs";
+import { ReplyBox, CommentList } from "../components/module/post/ReplyBox";
+import { CommentUser } from "../components/module/user/CommentUser";
 const PostView = () => {
   const dispatch = useDispatch();
+  const { register, handleSubmit } = useForm();
   // getPost API에 사용될 데이터
   const { postId } = useParams();
   // 댓글 생성시 useEffect에 적용하여, 재 렌더링 하기 위함
@@ -25,8 +50,17 @@ const PostView = () => {
   const { content, updatedAt, heartCount, commentCount, postImages } =
     useSelector(state => state.postRead);
 
+  console.log(postImages && postImages);
+
   // 댓글 리스트 배열을 스토어에서 가져오기
-  const commentList = useSelector(state => state.commentList.comments);
+  const commentListArr = useSelector(state => state.commentList.comments);
+
+  // 댓글 생성
+  const onSubmit = data => {
+    const { comment } = data;
+    console.log(comment);
+    dispatch(commentCreateAction(comment, postId));
+  };
 
   const onClickDeleteComment = commentId => {
     //댓글 삭제 API
@@ -48,12 +82,138 @@ const PostView = () => {
     dispatch(getCommentList(postId));
   }, [dispatch, postId, craeteCommentId, deleteCommentId]);
 
+  const [postDialog, setPostDialog] = useState(false);
+  const [postAlert, setPostAlert] = useState(false);
+  const isPostDialog = () => setPostDialog(!postDialog);
+  const isPostAlert = () => setPostAlert(!postAlert);
+
   return (
     <>
-      <h1>PostView</h1>
-      <h1>PostView</h1>
+      <HeaderHome />
+      <LayOut>
+        <Container>
+          {/* 유저 인포 */}
+          <UserInfoBox profileImage={image} name={username} id={accountname} />
+          {/* 게시글 영역 */}
+          <ContentBox content={content}>
+            <ImgContainer>
+              <ImgList>
+                {postImages &&
+                  postImages.map((postImage, index) => {
+                    return (
+                      <img key={index} src={postImage} alt="게시글 이미지" />
+                    );
+                  })}
+              </ImgList>
+              <ButtonList>
+                <button></button>
+              </ButtonList>
+            </ImgContainer>
+            <IconBox like={heartCount} comment={commentCount} />
+            <Date>{dayjs(updatedAt).format("YY년 MM월 DD일")}</Date>
+          </ContentBox>
+          <MoreBtn onClick={isPostDialog} />
+        </Container>
+        {/* 댓글 리스트*/}
+        <CommentList>
+          {commentListArr &&
+            commentListArr.map(user => {
+              console.log(user, "댓글 사용자");
+              return (
+                <ReplyBox
+                  img={user?.author?.image}
+                  username={user?.author?.username}
+                  time={"15분전"}
+                  comment={user?.content}
+                  key={user?.id}
+                />
+              );
+            })}
+        </CommentList>
+        {/* 댓글 생성 */}
+        <CommentLayOut>
+          <CommentSection>
+            <form onSubmit={handleSubmit(onSubmit)}>
+              <div>
+                <input
+                  name="comment"
+                  type="text"
+                  placeholder="댓글 입력하기"
+                  {...register("comment")}
+                />
+              </div>
+              <div>
+                <button>게시</button>
+              </div>
+            </form>
+          </CommentSection>
+        </CommentLayOut>
+      </LayOut>
+
+      {/* 게시글 Modal */}
+      <Modal visible={postDialog}>
+        <AlertBtn isAlert={isPostAlert}>신고하기</AlertBtn>
+        <ListBtn isDialog={isPostDialog}>모달창 닫기</ListBtn>
+      </Modal>
+      {/* 게시글 Alert */}
+      <Alert visible={postAlert} messageText="게시글을 신고하시겠어요?">
+        <AlertBox isAlert={isPostAlert}>예</AlertBox>
+        <AlertBox isAlert={isPostAlert}>아니요</AlertBox>
+      </Alert>
     </>
   );
 };
+
+const LayOut = styled.main`
+  ${props => props.theme.common.flexCenterColumn}
+  ${props =>
+    props.center &&
+    css`
+      justify-content: center;
+    `}
+  position: fixed;
+  height: calc(100% - 108px);
+  overflow-y: scroll;
+  min-width: 390px;
+  width: 100%;
+  min-width: 100%;
+  height: 100%;
+  margin-top: 48px;
+`;
+
+const Container = styled.article`
+  position: relative;
+  max-width: 358px;
+  width: 100%;
+  margin-bottom: 40px;
+`;
+const MoreBtn = styled.button`
+  position: absolute;
+  top: 4px;
+  right: 0;
+  width: 18px;
+  height: 18px;
+  background: url(${more}) no-repeat center / 18px 18px;
+  background-color: inherit;
+`;
+
+const CommentLayOut = styled.footer`
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  min-width: 390px;
+  width: 100%;
+  background-color: #fff;
+  z-index: 10;
+  border-top: 0.5px solid gainsboro;
+`;
+const CommentSection = styled.section`
+  position: relative;
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 60px;
+  padding: 0 16px;
+`;
 
 export default PostView;
