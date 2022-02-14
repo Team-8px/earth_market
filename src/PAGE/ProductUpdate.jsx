2a7c4a17d17fc90787fb188ef74090ed271305b1
jae <goawmfhfl1@naver.com>
2022-02-14 16:15:34 +0900

    Fix: Form Component

--- src/PAGE/ProductUpdate.jsx
+++ src/PAGE/ProductUpdate.jsx
@@ -68,66 +68,87 @@ const ProductUpdate = () => {
   };
   return (
     <>
-      <LayOut>
-        <FormContainerSection>
-          <FormContainer onSubmit={handleSubmit(onSubmit)}>
-            <HeaderLayOut>
-              <HeaderContainer>
-                <HeaderLinkImg src={PrevBtn} />
-                <Button width="90px" size="ms" color="#fff">
-                  저장
-                </Button>
-              </HeaderContainer>
-            </HeaderLayOut>
-            <HeaderButton />
-            <ProfileImgWrapper>
-              <label onChange={previewImage} htmlFor="profileImg">
-                <img
-                  src={isUpdatedImage ? updateImage : image}
-                  alt="프로필 사진"
-                />
-                <input
-                  type="file"
-                  accept="image/jpg,image/png,image/jpeg,image/gif"
-                  name="profileImg"
-                  id="profileImg"
-                  {...register("profileImg")}
-                ></input>
-              </label>
-            </ProfileImgWrapper>
-            <ProductFormWrapper>
-              <label>상품명</label>
-              <input
-                name="itemName"
-                type="text"
-                {...register("itemName")}
-                placeholder="2~10자 이내여야 합니다."
+      <Form onSubmit={handleSubmit(onSubmit)}>
+        {/* 헤더필드 영역 */}
+        <HeaderFieldSet>
+          <HeaderContainer>
+            <HeaderLinkImg src={PrevBtn} />
+            <Button width="90px" size="ms" color="#fff">
+              저장
+            </Button>
+          </HeaderContainer>
+          <HeaderButton />
+        </HeaderFieldSet>
+        {/* 메인필드 영역 */}
+        <MainFieldSet>
+          <ProfileImgWrapper>
+            <label onChange={previewImage} htmlFor="profileImg">
+              <img
+                src={isUpdatedImage ? updateImage : image}
+                alt="프로필 사진"
               />
-            </ProductFormWrapper>
-            <ProductFormWrapper>
-              <label>가격</label>
               <input
-                name="price"
-                type="text"
-                {...register("price")}
-                placeholder="숫자만 입력 가능합니다."
-              />
-            </ProductFormWrapper>
-            <ProductFormWrapper>
-              <label>판매 링크</label>
-              <input
-                name="link"
-                type="text"
-                {...register("link")}
-                placeholder="URL을 입력해 주세요."
-              />
-            </ProductFormWrapper>
-          </FormContainer>
-        </FormContainerSection>
-      </LayOut>
+                type="file"
+                accept="image/jpg,image/png,image/jpeg,image/gif"
+                name="profileImg"
+                id="profileImg"
+                {...register("profileImg")}
+              ></input>
+            </label>
+          </ProfileImgWrapper>
+          <ProductFormWrapper>
+            <label>상품명</label>
+            <input
+              name="itemName"
+              type="text"
+              {...register("itemName")}
+              placeholder="2~10자 이내여야 합니다."
+            />
+          </ProductFormWrapper>
+          <ProductFormWrapper>
+            <label>가격</label>
+            <input
+              name="price"
+              type="text"
+              {...register("price")}
+              placeholder="숫자만 입력 가능합니다."
+            />
+          </ProductFormWrapper>
+          <ProductFormWrapper>
+            <label>판매 링크</label>
+            <input
+              name="link"
+              type="text"
+              {...register("link")}
+              placeholder="URL을 입력해 주세요."
+            />
+          </ProductFormWrapper>
+        </MainFieldSet>
+      </Form>
     </>
   );
 };
+
+const Form = styled.form`
+  box-sizing: border-box;
+`;
+//  메인
+const MainFieldSet = styled.fieldset`
+  margin: 0 auto;
+  max-width: 390px;
+  width: 100%;
+  padding: 30px 34px 0;
+`;
+//  헤더
+const HeaderFieldSet = styled.fieldset`
+  position: fixed;
+  width: 100%;
+  min-width: 390px;
+  left: 0;
+  top: 0;
+  background-color: #fff;
+  z-index: 10;
+`;
 
 const LayOut = styled.main`
   display: flex;
@@ -137,16 +158,6 @@ const LayOut = styled.main`
   height: 100%;
   background-color: #fff;
   margin-top: 48px;
-`;
-
-const HeaderLayOut = styled.header`
-  position: fixed;
-  width: 100%;
-  min-width: 390px;
-  left: 0;
-  top: 0;
-  background-color: #fff;
-  z-index: 10;
 `;
 
 const HeaderContainer = styled.section`
@@ -167,18 +178,15 @@ const HeaderLinkImg = styled.img`
   cursor: pointer;
 `;
 
-const FormContainerSection = styled.section`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  max-width: 390px;
-  width: 100%;
-  padding: 30px 34px 0;
-`;
+// const FormContainerSection = styled.section`
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+//   max-width: 390px;
+//   width: 100%;
+//   padding: 30px 34px 0;
+// `;
 
-const FormContainer = styled.form`
-  box-sizing: border-box;
-`;
 const ProfileImgWrapper = styled.div`
   margin-top: 30px;
   margin-bottom: 16px;
