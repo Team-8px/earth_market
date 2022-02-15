e9aeaa194b6101887f792730ed9983760813ee72
jae <goawmfhfl1@naver.com>
2022-02-15 16:46:10 +0900

    Add: toggle 버튼 구현
    But 잘 모르겠습니다.
    on 태그를 더하는 방법을 모르겠습니다.

--- src/components/template/common/Navigation.js
+++ src/components/template/common/Navigation.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
 import { NavLink } from "react-router-dom";
 
@@ -51,8 +51,6 @@ const NavListLink = styled(NavLink)`
     background-repeat: no-repeat;
   }
   &.home::before {
-    /* public 폴더 안에 이미지 파일을 넣어서 사용할 경우 아래 주석처럼 코드를 줄일 수 있습니다! */
-    /* background-image: url("/img/icon/icon-home.svg"); */
     background-image: url(${HomeIcon});
   }
   &.chatlist::before {
@@ -65,46 +63,62 @@ const NavListLink = styled(NavLink)`
     background-image: url(${ProfileIcon});
   }
 
-  &:active {
-    color: #f26e22;
-    &.home::before {
-      background-image: url(${HomeIconFill});
-    }
-    &.chatlist::before {
-      background-image: url(${ChatIconFill});
-    }
-    &.myprofile::before {
-      background-image: url(${ProfileIconFill});
-    }
+  &.home .on ::before {
+    background-image: url(${HomeIconFill});
+  }
+  &.chatlist .on ::before {
+    background-image: url(${ChatIconFill});
+  }
+  &.myprofile .on ::before {
+    background-image: url(${ProfileIconFill});
   }
 `;
 
-export const NavList = ({ NavText, className, url }) => {
-  return (
-    <NavWrap>
-      <NavListLink to={url} className={className} exact>
-        {NavText}
-      </NavListLink>
-    </NavWrap>
-  );
-};
-
 const Navigation = () => {
+  const [toggle, setToggle] = useState(0);
+  console.log(toggle);
+  const toggleHandler = index => {
+    setToggle(index);
+  };
   return (
     <NavLayOut>
       <NavContainer>
-        <NavList url={"/home"} NavText={"홈"} className={"home"}></NavList>
-        <NavList url={"/chatlist"} NavText={"채팅"} className={"chatlist"} />
-        <NavList
-          url={"/post/my/upload"}
-          NavText={"게시물 작성"}
-          className={"upload"}
-        />
-        <NavList
-          url={"/profile/my"}
-          NavText={"프로필"}
-          className={"myprofile"}
-        />
+        <NavWrap>
+          <NavListLink
+            to="/home"
+            className={toggle === 1 ? "home on" : "home"}
+            onClick={() => toggleHandler(1)}
+          >
+            홈
+          </NavListLink>
+        </NavWrap>
+        <NavWrap>
+          <NavListLink
+            to="/home"
+            className={toggle === 2 ? "chatlist on" : "chatlist"}
+            onClick={() => toggleHandler(2)}
+          >
+            채팅
+          </NavListLink>
+        </NavWrap>
+        <NavWrap>
+          <NavListLink
+            to="/home"
+            className={toggle === 3 ? "upload on" : "upload"}
+            onClick={() => toggleHandler(3)}
+          >
+            게시물 작성
+          </NavListLink>
+        </NavWrap>
+        <NavWrap>
+          <NavListLink
+            to="/home"
+            className={toggle === 4 ? "myprofile on" : "myprofile"}
+            onClick={() => toggleHandler(4)}
+          >
+            프로필
+          </NavListLink>
+        </NavWrap>
       </NavContainer>
     </NavLayOut>
   );
