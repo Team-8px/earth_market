0a905ac78ba01c32a81ddbe8dff97b10bdea7f50
mijin <mmcho122@gmail.com>
2022-02-16 10:44:33 +0900

    Fix: Header position 속성 수정

--- src/components/template/common/Header.js
+++ src/components/template/common/Header.js
@@ -108,16 +108,16 @@ const SearchBtnWrap = () => {
 };
 
 const HeaderLayOut = styled.header`
-  position: fixed;
+  position: sticky;
   width: 100%;
+  height: 47.5px;
   min-width: 390px;
-  left: 0;
   top: 0;
   background-color: #fff;
   z-index: 10;
 `;
 
-const HeaderContainer = styled.section`
+const HeaderContainer = styled.div`
   position: relative;
   display: flex;
   justify-content: space-between;
@@ -137,7 +137,7 @@ const HeaderLink = styled(Link)`
   width: 22px;
   height: 22px;
   border: none;
-  margin-right: 10px;
+  /* margin-right: 10px; */
   cursor: pointer;
 `;
 
@@ -149,7 +149,7 @@ const HeaderLinkImg = styled.img`
 `;
 
 const HeaderTitle = styled.h2`
-  /* margin-left: 10px; */
+  margin-left: 8px;
   font-family: ${props =>
     props.fontWeigt === "400"
       ? "Spoqa Han Sans Neo R"
