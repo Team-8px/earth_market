6399eb457d90a2df2ebcdb6d3795f85d23aeea39
jisu2433 <93359953+jisu2433@users.noreply.github.com>
2022-02-16 11:47:22 +0900

    Add : profile 수정

--- src/PAGE/MyProfile.jsx
+++ src/PAGE/MyProfile.jsx
@@ -22,8 +22,8 @@ import Header from "../components/template/common/Header";
 import { ProductList, Product } from "../components/module/product/Product";
 import dayjs from "dayjs";
 import SellProductLink from "../asset/product-img-example-01.jpg";
-import { ProfileImage } from "../components/common/image/ProfileImageStyle";
-import { Button } from "../components/module/button/button";
+import UserInfo from "../components/module/profile/UserInfo";
+
 
 // import Product
 const MyProfile = () => {
@@ -83,32 +83,14 @@ const MyProfile = () => {
     <>
       <Header />
       <LayOut>
-      <UserInfoContainer>
-        <UserInfoWrapper>
-          <ProfileImage>
-            <img src={profileImage} alt="프로필 사진" />
-          </ProfileImage>
-          <UserName>{username}</UserName>
-          <AccountName>{accountname}</AccountName>
-          <Intro>{intro}</Intro>
-          <FollowerWrapper to={`/profile/${accountname}/follower`}>
-            <strong>{followerCount}</strong>
-            <span>followers</span>
-          </FollowerWrapper>
-          <FollowingWrapper to={`/profile/${accountname}/following`}>
-            <strong>{followingCount}</strong>
-            <span>following</span>
-          </FollowingWrapper>
-          <ButtonWrapper>
-          <Button size="md" width="120px">
-            프로필 수정
-          </Button>
-          <Button size="md" width="120px">
-            상품 등록
-          </Button>
-          </ButtonWrapper>
-        </UserInfoWrapper>
-      </UserInfoContainer>
+      <UserInfo
+        profileImage={profileImage}
+        username={username}
+        accoutname={accountname}
+        intro={intro}
+        followerCount={followerCount}
+        followingCount={followingCount}
+        />
         {/* <Product></Product>   */}
         <ProductLayOut>
           <Product>
@@ -311,94 +293,4 @@ const ProductPrice = styled.strong`
   color: #f26e22;
   font-weight: 700;
 `;
-const UserInfoContainer = styled.header`
-  display: flex;
-  justify-content: center;
-  border-bottom: 0.5px solid #DBDBDB;
-  background-color: #fff;
-  margin-bottom: 6px;
-`;
-const UserInfoWrapper = styled.div`
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  max-width: 390px;
-  width: 100%;
-  padding: 30px 16px 26px;
-
-  img {
-    margin-bottom: 16px;
-  }
-`;
-const UserName = styled.strong`
-  font-weight: 700;
-  font-size: 16px;
-  line-height: 20px;
-  margin-bottom: 6px;
-`;
-const AccountName = styled.strong`
-  font-weight: 400;
-  font-size: 12px;
-  line-height: 14px;
-  color: #767676;
-  margin-bottom: 16px;
-  &::before {
-    content: "@";
-    margin-right: 3px;
-  }
-`;
-const Intro = styled.p`
-  font-weight: 400;
-  font-size: 14px;
-  line-height: 18px;
-  color: #767676;
-  margin-bottom: 24px;
-`;
-const FollowerWrapper = styled(Link)`
-  position: absolute;
-  left: 56px;
-  top: 65px;
-  text-align: center;
-  cursor: pointer;
-
-  strong {
-    display: block;
-    font-weight: 700;
-    font-size: 18px;
-    line-height: 23px;
-    margin-bottom: 6px;
-  }
-
-  span {
-    font-size: 10px;
-    color: #767676;
-  }
-`;
-
-const FollowingWrapper = styled(Link)`
-  position: absolute;
-  left: 287px;
-  top: 65px;
-  text-align: center;
-  cursor: pointer;
-
-  strong {
-    display: block;
-    font-weight: 700;
-    font-size: 18px;
-    line-height: 23px;
-    margin-bottom: 6px;
-  }
-
-  span {
-    font-size: 10px;
-    color: #767676;
-  }
-`;
-
-const ButtonWrapper = styled.div`
-  display: flex;
-`;
-
 export default MyProfile;
