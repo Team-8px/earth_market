ef883cb5108f47f59591ce847e66643a00c05e2f
mijin <mmcho122@gmail.com>
2022-02-21 05:41:27 +0900

    Add: Splash 스타일 추가

--- src/PAGE/SplashScreen.jsx
+++ src/PAGE/SplashScreen.jsx
@@ -1,13 +1,22 @@
 import React from "react";
+import styled from "styled-components";
+import SplashLogo from "../asset/icon/full-logo.svg";
+
 
 const SplashScreen = () => {
   return (
     <>
-      <Header />
-      <Main />
-      <Navigation />
+      <SplashImg src={SplashLogo} />
     </>
   );
 };
+
+const SplashImg = styled.img`
+  display: block;
+  margin: 0 auto;
+  width: 200px;
+  /* vertical-align: middle; */
+  height: 100%;
+`;
 
 export default SplashScreen;
