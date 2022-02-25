bec8ba14a72ad31a8ce5a76e7a3f0da6ffc177ee
jisu2433 <93359953+jisu2433@users.noreply.github.com>
2022-02-25 12:58:46 +0900

    Fix : 숫자 정규표현식 수정

--- src/PAGE/ProductUpload.jsx
+++ src/PAGE/ProductUpload.jsx
@@ -132,7 +132,7 @@ const ProductUpload = () => {
             placeholder="숫자만 입력 가능합니다."
             autoComplete="off"
             spellCheck="false"
-            {...register("price", {required: true, pattern: /^[0-9]/g })}
+            {...register("price", {required: true, pattern: /^[0-9]*$/})}
           />
           {errors.price?.type === "pattern" && (<p>*숫자만 입력 가능합니다.</p>)}
           <label>판매 링크</label>
