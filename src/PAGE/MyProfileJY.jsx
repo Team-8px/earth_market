cb064ac45d0319051972cf3b8c9ffb9d23b03182
jae <goawmfhfl1@naver.com>
2022-02-28 17:11:37 +0900

    Add: 초기세팅

---
+++ src/PAGE/MyProfileJY.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ProfileContainer from "../components/Profile/ProfileContainer";
+
+const MyProfileJY = () => {
+  return <ProfileContainer />;
+};
+
+export default MyProfileJY;
---
+++ src/components/Profile/ProfileContainer/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { UserInfoContainer } from "./index.style";
+
+function ProfileContainer() {
+  return <UserInfoContainer>index</UserInfoContainer>;
+}
+
+export default ProfileContainer;
