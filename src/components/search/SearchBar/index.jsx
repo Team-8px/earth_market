8bb42fa6a4dd258ca42e9b7c3d998be527d09518
jae <goawmfhfl1@naver.com>
2022-03-02 15:43:24 +0900

    Add: SearchBar

--- src/components/search/SearchBar/index.jsx
+++ src/components/search/SearchBar/index.jsx
@@ -1,6 +1,28 @@
 import React from "react";
+import {
+  SearchHaeder,
+  SearchHaederContainer,
+  PrevBtn,
+  PrevIcon,
+  SearchForm,
+  SearchLabel,
+  SearchInput,
+} from "./index.style";
 
 function SearchBar() {
-  return <div>SearchBar</div>;
+  return (
+    <SearchHaeder>
+      <SearchHaederContainer>
+        <PrevBtn>
+          <PrevIcon />
+        </PrevBtn>
+        <SearchForm>
+          <SearchLabel>
+            <SearchInput />
+          </SearchLabel>
+        </SearchForm>
+      </SearchHaederContainer>
+    </SearchHaeder>
+  );
 }
 export default SearchBar;
--- src/components/search/SearchBar/index.style.js
+++ src/components/search/SearchBar/index.style.js
@@ -1,1 +1,70 @@
 import styled from "styled-components";
+import Prev from "../../../asset/icon-arrow-left.svg";
+
+export const SearchHaeder = styled.header`
+  position: fixed;
+  width: 100%;
+  min-width: 390px;
+  left: 0;
+  top: 0;
+  background-color: #fff;
+  z-index: 10;
+`;
+
+export const SearchHaederContainer = styled.section`
+  position: relative;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  max-width: 100%;
+  height: 48px;
+  padding: 0 16px;
+  border-bottom: 0.5px solid ${props => props.theme.palette["border"]};
+`;
+
+export const PrevBtn = styled.button.attrs({
+  type: "button",
+  alt: "이전 페이지 버튼",
+})`
+  width: 22px;
+  height: 22px;
+  border: none;
+  margin-right: 10px;
+  cursor: pointer;
+`;
+
+export const PrevIcon = styled.img.attrs({
+  src: Prev,
+})`
+  width: 100%;
+  height: 100%;
+`;
+
+export const SearchForm = styled.form`
+  width: 100%;
+`;
+
+export const SearchLabel = styled.label`
+  display: block;
+  font-size: 1px;
+`;
+export const SearchInput = styled.input.attrs({
+  name: "keyword",
+  type: "text",
+  placeholder: "계정검색",
+  autoComplete: "off",
+})`
+  width: 100%;
+  height: 32px;
+  background-color: ${props => props.theme.palette["bg"]};
+  border-style: none;
+  border-radius: 32px;
+  padding: 0 16px;
+  font-size: 14px;
+  color: ${props => props.theme.palette["main"]};
+  caret-color: ${props => props.theme.palette["main"]};
+
+  &::placeholder {
+    color: ${props => props.theme.palette["lightGray"]};
+  }
+`;
