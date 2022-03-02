fb41a59f3fbefaff453459bf83e7cc7219899d16
mijin <mmcho122@gmail.com>
2022-03-02 22:04:32 +0900

    Add: Upload 페이지

---
+++ src/PAGE/PostEditMJ.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import UploadHeader from "../components/Upload/UploadHeader";
+import EditPage from "../components/Upload/EditPage"
+
+
+const PostEditMJ = () => {
+  return (
+    <>
+      <UploadHeader/>
+      <EditPage /> 
+    </>
+  );
+};
+
+export default PostEditMJ;
