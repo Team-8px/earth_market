30371009ef1c21f658917a9b7aaaf814c1ce21b7
mijin <mmcho122@gmail.com>
2022-03-02 22:04:32 +0900

    Add: Upload 페이지

---
+++ src/PAGE/ProductUpdateMJ.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import UploadHeader from "../components/Upload/UploadHeader";
+import ProductEditPage from "../components/Upload/ProductEditPage"
+
+
+const ProductUpdateMJ = () => {
+  return (
+    <>
+      <UploadHeader/>
+      <ProductEditPage /> 
+    </>
+  );
+};
+
+export default ProductUpdateMJ;
---
+++ src/PAGE/ProductUploadMJ.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import UploadHeader from "../components/Upload/UploadHeader";
+import ProductPage from "../components/Upload/ProductEditPage"
+
+
+const ProductUploadMJ = () => {
+  return (
+    <>
+      <UploadHeader/>
+      <ProductPage /> 
+    </>
+  );
+};
+
+export default ProductUploadMJ;
