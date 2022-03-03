f061a4f69deee36aac6dc8e9ccf69dbd54d29cfb
mijin <mmcho122@gmail.com>
2022-03-03 17:06:05 +0900

    Add: Navbar component 스타일링

---
+++ src/components/common/Navbar/index.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { NavList, NavItem, StyledNavLink } from "./index.style";
+
+function Navigation() {
+  return (
+        <NavList>
+          <NavItem>
+            <StyledNavLink to="/" className="home">
+              홈
+            </StyledNavLink>
+          </NavItem>
+          <NavItem>
+            <StyledNavLink to="/chatlist" className="chatlist">
+              채팅
+            </StyledNavLink>
+          </NavItem>
+          <NavItem>
+            <StyledNavLink to="/post/upload" className="upload">
+              게시물 작성
+            </StyledNavLink>
+          </NavItem>
+          <NavItem>
+            <StyledNavLink to="/myprofile" className="myprofile">
+              프로필
+            </StyledNavLink>
+          </NavItem>
+        </NavList>
+  );
+}
+
+export default Navigation;
