906107c374f4786fdce9b59c3bb3b9031e9e1a61
jae <goawmfhfl1@naver.com>
2022-03-03 12:08:55 +0900

    Add: CommonHeader 적용
    CommonHeader 적용 완료하고 기존 MyProfile에 있던 내용들 정리했습니다.

--- src/PAGE/MyProfile.jsx
+++ src/PAGE/MyProfile.jsx
@@ -1,12 +1,5 @@
 import React, { useState } from "react";
 import styled from "styled-components";
-import { useHistory } from "react-router-dom";
-// 이미지 모음
-import prev from "../asset/icon-arrow-left.svg";
-import more from "../asset/icon-more-vertical.svg";
-
-import { Modal, AlertBtn, ListBtn } from "../components/module/modal/Modal";
-import { Alert, AlertBox } from "../components/module/alert/Alert";
 import Navigation from "../components/template/common/Navigation";
 
 // 작업완료
@@ -16,69 +9,27 @@ import ProductContainer from "../components/profile/ProductContainer";
 import GalleryContainer from "../components/profile/GalleryContainer";
 
 const MyProfile = () => {
-  const history = useHistory();
   // 좋아요
   const [isLikeAction, setLikeAction] = useState(false);
   console.log(isLikeAction && isLikeAction, "isLikeAction");
   const likeAction = () => setLikeAction(!isLikeAction);
-  // 🕹 네비게이션 Modal & Alert
-  const [navDialog, setNavDialog] = useState(false);
-  const [navAlert, setNavAlert] = useState(false);
-  const isNavDialog = () => setNavDialog(!navDialog);
-  const isNavAlert = () => setNavAlert(!navAlert);
-
   return (
     <>
       <CommonHeader />
-      <LayOut>
+      <MainSection>
         {/* 유저 프로필 */}
         <ProfileContainer />
         {/* 상품 */}
         <ProductContainer />
         {/* 게시글 */}
         <GalleryContainer />
-      </LayOut>
+      </MainSection>
       <Navigation />
-      <Modal visible={navDialog}>
-        <AlertBtn isAlert={isNavAlert}>로그아웃</AlertBtn>
-        <ListBtn isDialog={isNavDialog}>닫기</ListBtn>
-      </Modal>
-      {/* Nav Alert */}
-      <Alert visible={navAlert} messageText="로그아웃 하시겠어요?">
-        <AlertBox isAlert={isNavAlert}>예</AlertBox>
-        <AlertBox isAlert={isNavAlert}>아니요</AlertBox>
-      </Alert>
     </>
   );
 };
 
-const HeaderLayOut = styled.header`
-  position: sticky;
-  width: 100%;
-  height: 47.5px;
-  min-width: 390px;
-  top: 0;
-  background-color: #fff;
-  z-index: 10;
-`;
-const HeaderContainer = styled.div`
-  position: relative;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  max-width: 100%;
-  height: 48px;
-  padding: 0 16px;
-  border-bottom: 0.5px solid ${props => props.theme.palette["border"]};
-`;
-const HeaderLink = styled.div`
-  width: 22px;
-  height: 22px;
-  border: none;
-  /* margin-right: 10px; */
-  cursor: pointer;
-`;
-const LayOut = styled.main`
+const MainSection = styled.main`
   min-width: 390px;
   width: 100%;
   height: 100%;
