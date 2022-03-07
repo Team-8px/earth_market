b4d16a58170d1f17b97e0ccad8a8373f8931326d
jae <goawmfhfl1@naver.com>
2022-03-07 11:07:20 +0900

    Edit: location 수정

--- src/components/Login/LoginContainer/index.jsx
+++ src/components/Login/LoginContainer/index.jsx
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
 import LoginCard from "../LoginCard";
 import SplashCard from "../SplashCard";
 import { LoginContainerSection, LoginContainerWrapper } from "./index.style.js";
 
 function LoginContainer() {
   const [loading, setLoading] = useState(false);
+  const history = useHistory();
 
   useEffect(() => {
     if (JSON.parse(localStorage.getItem("userInfo"))?.user?.token) {
-      document.location.href = "/home";
+      history.push("/home");
     }
     // 로그인 상태일 경우 home 으로 가라
     setLoading(true);
