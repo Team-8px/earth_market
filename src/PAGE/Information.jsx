32e42ed861b8f74f2ed2f36bbdf8bda2de6d904a
jae <goawmfhfl1@naver.com>
2022-03-08 16:34:52 +0900

    Add: 문서 불러오기 테스트

--- src/PAGE/Information.jsx
+++ src/PAGE/Information.jsx
@@ -1,16 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import axios from "axios";
+import XMLParser from "react-xml-parser";
 
 const ChatList = () => {
+  const [data, setData] = useState([]);
   const getAPI = async () => {
     const response = await axios.get(
-      "http://api.nongsaro.go.kr/service/monthFarmTech/monthFarmTechLst?apiKey=20220307MWUDEDKBKRX9E0GEEJ1IYA",
+      "https://cors-anywhere.herokuapp.com/http://api.nongsaro.go.kr/service/monthFarmTech/monthFarmTechLst?apiKey=20220307MWUDEDKBKRX9E0GEEJ1IYA",
     );
-    console.log(response.data);
+    const dataSet = response.data;
+    const dataArr = new XMLParser().parseFromString(dataSet).children[1]
+      .children[0].children;
+    setData(dataArr);
   };
+  console.log(data);
+
   return (
     <>
-      <button onClick={getAPI}>버튼</button>
+      <button onClick={getAPI}>click</button>
+      <ul>
+        {data &&
+          data.map((item, index) => {
+            return (
+              <li key={Math.random() * 100}>
+                {item.children.map(items => {
+                  return <div>test</div>;
+                })}
+              </li>
+            );
+          })}
+      </ul>
     </>
   );
 };
