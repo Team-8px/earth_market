556cde17e7135de609eb66794bdc4d943dc5e274
jisu2433 <93359953+jisu2433@users.noreply.github.com>
2022-03-09 16:38:20 +0900

    Edit : 불필요한 이미지 삭제

--- src/components/module/button/button.js
+++ src/components/module/button/button.js
@@ -1,7 +1,6 @@
 // 기존 로직 적용
 import React from "react";
 import styled, { css } from "styled-components";
-import faceBook from "../../../asset/facebook.png";
 import chatImage from "../../../asset/icon/icon-message-circle.png";
 import shareImage from "../../../asset/icon/icon-share.png";
 
