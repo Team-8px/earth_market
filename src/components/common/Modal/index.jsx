ba69d09b1d20ba4fc4f893970edd51956a2d7f38
jae <goawmfhfl1@naver.com>
2022-03-10 14:54:42 +0900

    Add: Modal 컴포넌트 추가

--- src/components/common/Modal/index.jsx
+++ src/components/common/Modal/index.jsx
@@ -1,1 +1,37 @@
+import React, { useEffect, useState } from "react";
+import { ModalContainer, ModalList, ModalItem } from "./index.style";
 
+export function ListBtn({ children, isDialog }) {
+  return (
+    <ModalItem>
+      <button type="button" onClick={isDialog}>
+        {children}
+      </button>
+    </ModalItem>
+  );
+}
+export function Modal({ children, visible }) {
+  const [animate, setAnimate] = useState(false);
+  const [localVisible, setLocalVisible] = useState(visible);
+
+  useEffect(() => {
+    if (localVisible && !visible) {
+      setAnimate(true);
+      setTimeout(() => setAnimate(false), 200);
+    }
+
+    setLocalVisible(visible);
+  }, [localVisible, visible]);
+
+  if (!localVisible && !animate) return null;
+  return (
+    <ModalContainer disappear={!visible}>
+      <ModalList disappear={!visible}>{children}</ModalList>
+    </ModalContainer>
+  );
+}
+
+Modal.defaultProps = {
+  settingText: "사용자 설정 및 프로필",
+  logoutText: "로그아웃",
+};
