6a82b220ef6615ed7bf6ee6e5bf83071ade22fbb
jae <goawmfhfl1@naver.com>
2022-03-10 16:21:17 +0900

    Refactoring: NotFoundContainer
    컨테이너 스타일 컴포넌트 합체시켰습니다.

--- src/components/etc/NotFoundContainer/index.jsx
+++ src/components/etc/NotFoundContainer/index.jsx
@@ -1,23 +1,30 @@
 import React from "react";
 import { useHistory } from "react-router-dom";
-import { NotFoundIcon, NotFoundSubText, PrevBtn } from "./index.style";
+import {
+  NotFoundSection,
+  NotFoundIcon,
+  NotFoundSubText,
+  PrevBtn,
+} from "./index.style";
 
-function NotFound() {
+function NotFoundContainer() {
   const history = useHistory();
 
   return (
     <>
-      <NotFoundIcon />
-      <NotFoundSubText>페이지를 찾을 수 없습니다.</NotFoundSubText>
-      <PrevBtn
-        onClick={() => {
-          history.goBack();
-        }}
-      >
-        이전 페이지
-      </PrevBtn>
+      <NotFoundSection>
+        <NotFoundIcon />
+        <NotFoundSubText>페이지를 찾을 수 없습니다.</NotFoundSubText>
+        <PrevBtn
+          onClick={() => {
+            history.goBack();
+          }}
+        >
+          이전 페이지
+        </PrevBtn>
+      </NotFoundSection>
     </>
   );
 }
 
-export default NotFound;
+export default NotFoundContainer;
