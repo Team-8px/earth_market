85166dc20c1606df946c808f861e383ff1d755e3
jae <goawmfhfl1@naver.com>
2022-03-10 15:36:12 +0900

    Edit: 홈 Modal & Alert 모듈 분리

--- src/components/home/HomeContainer/index.jsx
+++ src/components/home/HomeContainer/index.jsx
@@ -2,8 +2,8 @@ import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { getFollowerPostList } from "../../../actions/followAction";
 import { useDispatch, useSelector } from "react-redux";
-import { Modal, AlertBtn, ListBtn } from "../../module/modal/Modal";
-import { Alert, AlertBox } from "../../module/alert/Alert";
+import { Modal, ModalListBtn, ModalAlertBtn } from "../../common/Modal";
+import { Alert, AlertBtn } from "../../common/Alert";
 import { ProfileSection, NoneFeed, SearchBtn } from "./index.style";
 import { changeDefaltImage } from "../../../util/changeDefaltImage";
 import PostCard from "../../common/PostCard";
@@ -56,12 +56,12 @@ function HomeContainer() {
         </ProfileSection>
       )}
       <Modal visible={postDialog}>
-        <AlertBtn isAlert={isPostAlert}>신고하기</AlertBtn>
-        <ListBtn isDialog={isPostDialog}>모달창 닫기</ListBtn>
+        <ModalAlertBtn isAlert={isPostAlert}>신고하기</ModalAlertBtn>
+        <ModalListBtn isDialog={isPostDialog}>모달창 닫기</ModalListBtn>
       </Modal>
       <Alert visible={postAlert} messageText="게시글을 신고하시겠어요?">
-        <AlertBox isAlert={isPostAlert}>예</AlertBox>
-        <AlertBox isAlert={isPostAlert}>아니요</AlertBox>
+        <AlertBtn isAlert={isPostAlert}>예</AlertBtn>
+        <AlertBtn isAlert={isPostAlert}>아니요</AlertBtn>
       </Alert>
     </>
   );
