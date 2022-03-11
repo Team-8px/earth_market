4a29a52498109307ff82e5a7032535f9cffed2c0
hyunghilkim <gusrlf14@gmail.com>
2022-03-11 16:48:10 +0900

    add: profile 부분 팔로우, 언팔로우 기능 추가

--- src/components/follow/FollowerContainer/index.jsx
+++ src/components/follow/FollowerContainer/index.jsx
@@ -9,33 +9,32 @@ import {
 import { FollowerSection, FollowerList } from "./index.style";
 import FollowerCard from "../FollowerCard";
 import { changeDefaltImage } from "../../../util/changeDefaltImage";
-function FollowerContainer() {
-  //   followerlistjy
-  const dispatch = useDispatch();
-  const { accountId } = useParams();
-  const { follower } = useSelector(state => state.followerList);
 
-  console.log(follower);
+const FollowerContainer = () => {
+  const dispatch = useDispatch();
+
+  const { accountId } = useParams();
+
+  const { follower } = useSelector(state => state.followerList);
 
   const onUnfollowClick = otherAccountId => {
     dispatch(unfollowUser(otherAccountId));
   };
+
   const onFollowClick = otherAccountId => {
     dispatch(followUser(otherAccountId));
   };
-  //readux스토어에서 팔로우취소여부를 불러와 useEffect에서 재 렌더링을 하기 위한 의도
+
   const { unfollow } = useSelector(state => state?.unfollowUser);
-  //readux스토어에서 팔로우등록여부를 불러와 useEffect에서 재 렌더링을 하기 위한 의도
+
   const { follow } = useSelector(state => state?.followUser);
 
   useEffect(() => {
-    // 테스트 라우팅
     dispatch(getFollowerList(accountId));
   }, [dispatch, unfollow, follow]);
-  
+
   return (
     <FollowerSection>
-      {/* ul태그 */}
       <FollowerList>
         {follower &&
           follower.map(followerUser => {
@@ -56,6 +55,6 @@ function FollowerContainer() {
       </FollowerList>
     </FollowerSection>
   );
-}
+};
 
 export default FollowerContainer;
