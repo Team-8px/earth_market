91139b634b301e988d3721f65f06c44ba933c707
mijin <mmcho122@gmail.com>
2022-03-11 23:43:55 +0900

    Add: InformationContainer 추가

--- src/components/information/InformationContainer/index.jsx
+++ src/components/information/InformationContainer/index.jsx
@@ -1,15 +1,23 @@
 import React, { useState } from "react";
+import InformationCard from "../InformationCard";
 import axios from "axios";
 import usePromise from "../../../hooks/usePromise";
+import { timeForToday } from "../../../util/timeForToday";
+import { InfomationWrapper, InfomationList } from "./index.style.js"
+
+
 import Loading from "../../etc/Loading";
 import Error from "../../etc/Error";
 
-const InformationContainer = () => {
+const InformationContainer = (time) => {
   const [loading, response, error] = usePromise(() => {
-    return axios.get(
+    const data = axios.get(
       "http://api.nongsaro.go.kr/service/monthFarmTech/monthFarmTechLst?apiKey=20220307MWUDEDKBKRX9E0GEEJ1IYA",
-    );
+    ); 
+    console.log(response) 
+    return data
   });
+  console.log(response) 
 
   if (loading) {
     return <Loading />;
@@ -19,26 +27,24 @@ const InformationContainer = () => {
   }
 
   return (
-    <>
-      <ul>
+    <InfomationWrapper>
+      <InfomationList>
         {response &&
-          response.map(item => {
-            console.log("item List", item.children);
+          response.map((item,i) => {
             return (
-              <ul key={Math.random() * 100}>
-                <div>{item.children[0].value}</div>
-                <div>{item.children[7].value}</div>
-                <img
-                  src={`http://www.nongsaro.go.kr/portal/imgView.do?atchmnflGroupEsntlCode=${item.children[0].value.slice(
-                    0,
-                    -1,
-                  )}`}
+                <InformationCard
+                  key={Math.random() * 100}
+                  newstitle={item.children[5].value.slice(0,-1)}
+                  newsimage={`http://www.nongsaro.go.kr/portal/imgView.do?atchmnflGroupEsntlCode=${item.children[0].value.slice(
+                        0,
+                        -1,
+                      )}`}
+                  newsdate={timeForToday(item.children[11].value.slice(0,-1))}
                 />
-              </ul>
             );
           })}
-      </ul>
-    </>
+      </InfomationList>
+    </InfomationWrapper>
   );
 };
 
---
+++ src/components/information/InformationContainer/index.style.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const InfomationWrapper = styled.article`
+  display: flex;
+  justify-content: center;
+  padding-bottom: 80px;
+`;
+
+export const InfomationList = styled.ul`
+  position: relative;
+  width: 100%;
+  padding: 20px 16px 0;
+  height: calc(100% - 108px);
+  overflow-y: scroll;
+  max-width: 450px;
+  margin-top: 20px;
+`;
