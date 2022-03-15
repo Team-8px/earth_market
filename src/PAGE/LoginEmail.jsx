6b815eed93a3f94d8aea76c2f85bf38c88253270
hyunghilkim <gusrlf14@gmail.com>
2022-03-15 12:36:28 +0900

    add: 이미지 미리보기 삭제 기능 추가

---
+++ src/PAGE/LoginEmail.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import LoginEmailForm from "../components/Login/LoginEmail";
+
+const LoginEmail = () => {
+  return <LoginEmailForm />;
+};
+
+export default LoginEmail;
