7094cde9eccaf8fbc025599315f9b16248b87fca
mijin <mmcho122@gmail.com>
2022-03-16 13:11:27 +0900

    Edit: LoginEmail 마크업 변경

--- src/components/Login/LoginEmail/index.jsx
+++ src/components/Login/LoginEmail/index.jsx
@@ -3,12 +3,14 @@ import { useForm } from "react-hook-form";
 import { useDispatch } from "react-redux";
 import { login } from "../../../actions/userActions";
 import {
-  Form,
-  MainFieldSet,
-  LoginTitle,
-  InputWrapper,
-  LoginText,
+  LoginEmailForm,
+  LoginEmailTitle,
+  LoginEmailFieldSet,
+  LoginEmailLabel,
+  LoginEmailInput,
+  ErrorMessage,
   LoginButton,
+  LoginText,
 } from "./index.style";
 
 const LoginEmail = () => {
@@ -41,62 +43,54 @@ const LoginEmail = () => {
   };
 
   return (
-    <>
-      <Form onSubmit={handleSubmit(onSubmit)}>
-        <MainFieldSet>
-          <LoginTitle>로그인</LoginTitle>
-          <InputWrapper>
-            <label>
-              이메일
-              <input
-                name="email"
-                type="email"
-                placeholder="이메일 주소를 입력해 주세요."
-                autoComplete="off"
-                spellCheck="false"
-                {...register("email", {
-                  required: true,
-                  pattern: /^[a-zA-Z0-9+-\_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-                })}
-              />
-              {errors?.email?.type === "required" && (
-                <p>* 필수 입력사항입니다.</p>
-              )}
-              {errors.email?.type === "pattern" && (
-                <p>*올바르지 않은 이메일 형식입니다.</p>
-              )}
-            </label>
-            <label>
-              비밀번호
-              <input
-                name="password"
-                type="password"
-                spellCheck="false"
-                placeholder="비밀번호를 설정해 주세요."
-                {...register("password", { required: true, minLength: 6 })}
-              />
-              {errors?.password?.type === "required" && (
-                <p>* 필수 입력사항입니다.</p>
-              )}
-              {errors.password?.type === "minLength" && (
-                <p>*비밀번호는 6자 이상이어야 합니다.</p>
-              )}
-              {notMatchError && <p>{notMatchError}</p>}
-            </label>
-          </InputWrapper>
-          <LoginButton
-            disabled={!isValid}
-            type="submit"
-            width="322px"
-            size="lg"
-            isValid={isValid}
-          >
-            로그인
-          </LoginButton>
-          <LoginText to="/join">이메일로 회원가입</LoginText>
-        </MainFieldSet>
-      </Form>
-    </>
+    <LoginEmailForm onSubmit={handleSubmit(onSubmit)}>
+      <LoginEmailTitle>로그인</LoginEmailTitle>
+      <LoginEmailFieldSet>
+        <LoginEmailLabel htmlFor="email">
+          이메일
+          <LoginEmailInput
+            name="email"
+            type="email"
+            placeholder="이메일 주소를 입력해 주세요."
+            autoComplete="off"
+            spellCheck="false"
+            {...register("email", {
+              required: true,
+              pattern: /^[a-zA-Z0-9+-\_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+            })}
+          />
+          {errors?.email?.type === "required" && (
+            <ErrorMessage>* 필수 입력사항입니다.</ErrorMessage>
+          )}
+          {errors.email?.type === "pattern" && (
+            <ErrorMessage>*올바르지 않은 이메일 형식입니다.</ErrorMessage>
+          )}
+        </LoginEmailLabel>
+        <LoginEmailLabel>
+          비밀번호
+          <LoginEmailInput
+            name="password"
+            type="password"
+            spellCheck="false"
+            placeholder="비밀번호를 설정해 주세요."
+            {...register("password", { required: true, minLength: 6 })}
+          />
+          {errors?.password?.type === "required" && (
+            <ErrorMessage>* 필수 입력사항입니다.</ErrorMessage>
+          )}
+          {errors.password?.type === "minLength" && (
+            <ErrorMessage>*비밀번호는 6자 이상이어야 합니다.</ErrorMessage>
+          )}
+          {notMatchError && <ErrorMessage>{notMatchError}</ErrorMessage>}
+        </LoginEmailLabel>
+        <LoginButton
+          disabled={!isValid}
+        >
+          로그인
+        </LoginButton>
+        <LoginText to="/join">이메일로 회원가입</LoginText>
+      </LoginEmailFieldSet>
+    </LoginEmailForm>
   );
 };
 
