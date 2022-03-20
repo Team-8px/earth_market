9ce266c4de384a1d7ff09033ab6e0a50e00a9c1d
mijin <mmcho122@gmail.com>
2022-03-20 15:07:53 +0900

    Style: Information 페이지 반응형 및 스타일링

--- src/components/information/InformationCard/index.style.js
+++ src/components/information/InformationCard/index.style.js
@@ -4,7 +4,7 @@ export const InformationItem = styled.li`
   display: flex;
   justify-content: center;
   align-items: center;
-  padding: 30px 20px;
+  padding: 1.8rem 1.25rem;
   border-bottom: 1px solid ${props => props.theme.palette["border"]};
   &:last-child {
     border-bottom: none;
@@ -12,8 +12,11 @@ export const InformationItem = styled.li`
 `;
 
 export const InformationWrapper = styled.article`
-  max-width: 800px;
+  max-width: 50rem;
   width: 100%;
+  &:hover {
+    opacity: 0.5;
+  }
 `;
 
 export const InformationLink = styled.div`
@@ -21,55 +24,55 @@ export const InformationLink = styled.div`
 `;
 
 export const InformationImg = styled.img`
-  width: 160px;
-  height: 105px;
+  width: 14rem;
+  height: 6.8rem;
   border-radius: 8px;
   object-fit: cover;
   background: ${props => props.theme.palette["border"]};
+  margin-right: 1.25rem;
+
+  @media only screen and (max-width: 750px) {
+    width: 9.25rem;
+    margin-right: 1rem;
+  }
 `;
 
 export const InformationInfoWrapper = styled.div`
-  width: calc(100% - 160px);
-  min-width: 275px;
-  margin-left: 20px;
+  width: calc(100% - 10rem);
+  min-width: 50%;
 `;
 
 export const InformationInfo = styled.span`
   display: inline-block;
-  font-size: 12px;
+  font-size: 0.75rem;
   color: ${props => props.theme.palette["main"]};
 `;
 
 export const InformationDate = styled.span`
-  font-size: 12px;
+  font-size: 0.75rem;
   font-weight: 400;
-  line-height: 13px;
+  line-height: 1.2;
   color: ${props => props.theme.palette["main"]};
-  margin-top: 8.5px;
+  margin-top: 0.5rem;
   &::before {
     content: "·";
-    margin: 0 4px;
+    margin: 0 .25rem;
   }
 `;
 
 export const InformationTitle = styled.strong`
   display: block;
   margin: 5px 0 10px;
-  font-size: 20px;
+  font-size: 1.25rem;
   font-weight: 600;
   line-height: 1.6;
   white-space: nowrap;
   overflow: hidden;
   text-overflow: ellipsis;
-  &:hover {
-    text-decoration: underline;
-    text-underline-position: under;
-    text-decoration-color: ${props => props.theme.palette["main"]};
-  }
 `;
 
 export const InformationText = styled.p`
-  font-size: 14px;
+  font-size: 0.8rem;
   line-height: 1.6;
   overflow: hidden;
   color: ${props => props.theme.palette["subText"]};
